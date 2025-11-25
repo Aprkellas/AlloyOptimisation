0c5c1e91f15ba999f883daf2776d28dfc87053c1
Alex Kellas <alex.kellas.relton@gmail.com>
2025-11-25 10:16:30 +0000

    Creep resistance test written

---
+++ AlloyOptimsation/AlloyOptimisation.Domain/Calculators/CreepResistanceCalculator.cs
@@ -0,0 +1,12 @@
+﻿using AlloyOptimisation.Domain.Alloy;
+
+namespace AlloyOptimisation.Domain.Calculators
+{
+    public class CreepResistanceCalculator : ICreeResistanceCalculator
+    {
+        public double Compute(AlloyComposition composition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- AlloyOptimsation/AlloyedOptimisation.Tests/CreepResistanceCalculatorTests.cs
+++ AlloyOptimsation/AlloyedOptimisation.Tests/CreepResistanceCalculatorTests.cs
@@ -1,4 +1,6 @@
-using AlloyOptimisation.Domain;
+using AlloyOptimisation.Domain.Alloy;
+using AlloyOptimisation.Domain.Calculators;
+using AlloyOptimisation.Domain.Elements;
 
 namespace AlloyOptimsationTests
 {
@@ -10,6 +12,23 @@ namespace AlloyOptimsationTests
             var ni = new ElementDefinition("Ni", alpha: 0.0, costPerKg: 10.0);
             var cr = new ElementDefinition("Cr", alpha: 1.23, costPerKg: 5.0);
             var co = new ElementDefinition("Co", alpha: 2.34, costPerKg: 7.0);
+
+            var system = new AlloySystem(
+                baseElement: ni,
+                variableElements: [cr, co]
+            );
+
+            var composition = new AlloyComposition(system, new Dictionary<ElementDefinition, double>
+            {
+                [cr] = 10.0,
+                [co] = 5.0
+            });
+
+            var calculator = new CreepResistanceCalculator();
+
+            var creep = calculator.Compute(composition);
+
+            //Assert.InRange(creep, expected * 0.999, expected * 1.001);
         }
     }
 }
