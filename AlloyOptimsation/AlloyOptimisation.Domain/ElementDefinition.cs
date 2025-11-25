bd578e4e22ab7af43b29ff34039396ee6be24d6e
Alex Kellas <alex.kellas.relton@gmail.com>
2025-11-25 09:50:43 +0000

    ElementDefintion class added and creep resistance test started

---
+++ AlloyOptimsation/AlloyOptimisation.Domain/ElementDefinition.cs
@@ -0,0 +1,16 @@
+﻿namespace AlloyOptimisation.Domain
+{
+    public class ElementDefinition
+    {
+        public string Symbol { get; }
+        public double Alpha { get; }
+        public double CostPerKg { get; }
+
+        public ElementDefinition(string symbol, double alpha, double costPerKg)
+        {
+            Symbol = symbol;
+            Alpha = alpha;
+            CostPerKg = costPerKg;
+        }
+    }
+}
