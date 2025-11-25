a73df746def32c06e582e119e65476b129c34ec9
Alex Kellas <alex.kellas.relton@gmail.com>
2025-11-25 10:03:41 +0000

    AlloyComposition and AlloySystem class's made

---
+++ AlloyOptimsation/AlloyOptimisation.Domain/AlloyComposition.cs
@@ -0,0 +1,16 @@
+﻿namespace AlloyOptimisation.Domain
+{
+    public class AlloyComposition
+    {
+        public AlloySystem System { get; }
+        public IReadOnlyDictionary<ElementDefinition, double> AtomicPercents { get; }
+        public double BaseElementPercent => 100.0 - AtomicPercents.Values.Sum();
+
+        public AlloyComposition(AlloySystem system,
+                                IDictionary<ElementDefinition, double> atomicPercents)
+        {
+            System = system;
+            AtomicPercents = new Dictionary<ElementDefinition, double>(atomicPercents);
+        }
+    }
+}
---
+++ AlloyOptimsation/AlloyOptimisation.Domain/AlloySystem.cs
@@ -0,0 +1,15 @@
+﻿namespace AlloyOptimisation.Domain
+{
+    public class AlloySystem
+    {
+        public ElementDefinition BaseElement { get; }
+        public IReadOnlyList<ElementDefinition> VariableElements { get; }
+
+        public AlloySystem(ElementDefinition baseElement,
+                           IReadOnlyList<ElementDefinition> variableElements)
+        {
+            BaseElement = baseElement;
+            VariableElements = variableElements;
+        }
+    }
+}
