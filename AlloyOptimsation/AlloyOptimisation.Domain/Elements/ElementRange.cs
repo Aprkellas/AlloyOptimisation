387db11afc3f84b1dff14adfa0220903cc8e556d
Alex Kellas <alex.kellas.relton@gmail.com>
2025-11-25 10:41:27 +0000

    Element range class added for enumeration test

---
+++ AlloyOptimsation/AlloyOptimisation.Domain/Elements/ElementRange.cs
@@ -0,0 +1,22 @@
+﻿namespace AlloyOptimisation.Domain.Elements
+{
+    public class ElementRange
+    {
+        public ElementDefinition Element { get; }
+        public double Min { get; }
+        public double Max { get; }
+        public double Step { get; }
+
+        public ElementRange(
+            ElementDefinition element, 
+            double min, 
+            double max, 
+            double step)
+        {
+            Element = element ?? throw new ArgumentNullException(nameof(element));
+            Min = min;
+            Max = max;
+            Step = step;
+        }
+    }
+}
